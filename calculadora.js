db684c61f10aecdc7bbe579771dfc1033be82284
Izadora <izadora.pelizario@edu.senai.br>
2026-04-08 10:48:08 -0300

    Meu primeiro envio

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+
+num1 = prompt(input("Digite o primeiro número:"))
+num2 = prompt(input("Digite o segundo número:"))
+
+console.log("Soma:", num1 + num2)
